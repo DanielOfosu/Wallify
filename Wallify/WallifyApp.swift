3c987ddadb37d8c044d5d67b6d1c6304cd98470f
DanielOfosu <daniel.ofosu@wolt.com>
2025-06-22 16:58:49 +0300

    Initial Commit

---
+++ Wallify/WallifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallifyApp.swift
+//  Wallify
+//
+//  Created by Daniel Ofosu on 22.6.2025.
+//
+
+import SwiftUI
+
+@main
+struct WallifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
